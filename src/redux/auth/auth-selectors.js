75c9a311902fe36c5f844329285be198a42837bd
Knirta <myknirtak@gmail.com>
2022-02-03 14:30:51 +0200

    add project files

---
+++ src/redux/auth/auth-selectors.js
@@ -0,0 +1,6 @@
+export const getIsAuthenticated = (state) => state.auth.isAuthenticated;
+export const getUserName = (state) => state.auth.user.name;
+export const getBalance = (state) => state.auth.user.balance;
+export const getUser = (state) => state.auth.user;
+export const getError = (state) => state.auth.error;
+export const getIsFetchingCurrentUser = (state) => state.auth.getIsFetchingCurrentUser;
